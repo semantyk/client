bc2c837c3d9a6238e047e8b972599a81f9247324
Daniel B <danielbakas@gmail.com>
2022-07-13 02:29:36 -0500

    Add documentation and General Cleanup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 <h1 align='center'>Semantyk</h1>
 <h3 align='center'><i>Ideas Wonder</i></h3>
 <p align='center'>
-  <img alt='Semantyk' src='https://www.semantyk.com/Semantyk.png' width='250'/>
+  <img alt='Semantyk' src='https://www.semantyk.com/logo.png' width='250'/>
   <br>
   <br>
   <b>August 31, 2017</b>
--- src/App.test.ts
+++ src/App.test.ts
@@ -1,9 +1,23 @@
+/**
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # semantyk.com
+ * Module | `App.test.tsx`
+ *
+ * June 13, 2022
+ *
+ * Copyright © Semantyk 2022. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* External Exports
 import {render} from '@testing-library/react';
 
+//* Internal Exports
 import App from "./App";
 
-describe("GET /", () => {
-    it("status: 200 OK", () => {
+//* Main
+describe("Render", () => {
+    it("<App />", () => {
         render(App());
     });
 });
