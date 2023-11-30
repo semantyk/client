540a1104e35f542076df053dddc2c9035bf3fbb6
Daniel B <danielbakas@gmail.com>
2023-11-30 05:14:10 -0600

    📓 Docs: Add header

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
-export default function Home() {
-    return (
-        <main>
-            Semantyk
-        </main>
-    );
+/*
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `page.js` | `app`
+ * README.md | Semantyk
+ *
+ * Created: Nov 30, 2023
+ * Modified: Nov 30, 2023
+ *
+ * Author(s): Semantyk Team
+ * Maintainer(s): Daniel Bakas <https://id.danielbakas.com>
+ *
+ * Copyright © Semantyk 2023. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Main
+export default async function Main() {
+    return (<></>);
 }
