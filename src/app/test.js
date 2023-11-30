18dfa0223f6d4de9e6afac8a36af15d1791dbfb1
Daniel B <danielbakas@gmail.com>
2023-11-30 05:17:25 -0600

    🧪 Test: Add test for Main

---
+++ src/app/test.js
@@ -0,0 +1,24 @@
+/*
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `test.js` | `app`
+ * README.md | Semantyk
+ *
+ * Created: Nov 30, 2023
+ * Modified: Nov 30, 2023
+ *
+ * Author(s): Semantyk Team
+ * Maintainer(s): Daniel Bakas <https://id.danielbakas.com>
+ *
+ * Copyright © Semantyk 2023. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Imports
+import { render } from "@testing-library/react";
+//* Local Imports
+import Main from "./page";
+
+//* Main
+test("renders component correctly", () => {
+    render(<Main/>);
+});
