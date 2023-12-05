0f43c17c8b612b8c4927273924a10d0358858bd2
Daniel B <danielbakas@gmail.com>
2023-12-05 04:08:00 -0600

    🧹 Chore: Separate logic for login

---
+++ src/logic/auth/service.js
@@ -0,0 +1,25 @@
+/*
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `service.js` | `auth`
+ * client | Semantyk
+ *
+ * Created: Dec 04, 2023
+ * Modified: Dec 04, 2023
+ *
+ * Author(s): Semantyk Team
+ * Maintainer(s):
+ *
+ * Copyright © Semantyk 2023. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Imports
+import { CLIENT_ID, POD_URI } from "@/services/app/nodes";
+
+export async function getOptions() {
+    return {
+        authOptions: { clientId: CLIENT_ID },
+        oidcIssuer: POD_URI,
+        redirectUrl: window.location.href
+    };
+}
