ef6bf55f81c90e2f0d8e112ed2fcb174a5af3693
Daniel B <danielbakas@gmail.com>
2023-12-05 04:02:42 -0600

    🧹 Chore: Rename getAppData logic to getData

--- src/logic/data/service.js
+++ src/logic/data/service.js
@@ -1,24 +1,38 @@
 /*
  * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
- * # `service.js` | `data`
+ * # `service.js`
  * client | Semantyk
  *
- * Created: Dec 03, 2023
- * Modified: Dec 03, 2023
+ * Created: Dec 05, 2023
+ * Modified: Dec 05, 2023
  *
  * Author(s): Semantyk Team
- * Maintainer(s): Daniel Bakas <https://id.danielbakas.com>
+ * Maintainer(s):
  *
  * Copyright © Semantyk 2023. All rights reserved.
  * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
  */
 
 //* Main
-import getAppData from "./appData/service";
+import { getProperties, getThing } from "@/services/solid/service";
+import { APP_WEBID, APP_WEBID_DOC } from "@/services/app/nodes";
+import { appShape } from "@/services/app/shape";
 
-export default function getData() {
-    // Hooks
-    const app = getAppData();
+//* Main
+export async function getData(fetch) {
+    // Logic
+    const thing = await getThing(fetch, APP_WEBID_DOC, APP_WEBID);
+    const lang = navigator.language.substring(0, 2);
+    const properties = getProperties(thing, appShape, lang);
+    // Props
+    const team = "Semantyk Team";
+    const twitter = "@semantyk";
     // Return
-    return { app };
+    return {
+        author: team,
+        creator: team,
+        lang,
+        twitter,
+        ...properties
+    };
 }
