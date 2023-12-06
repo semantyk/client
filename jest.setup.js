9548e761b9e72e6aa1ecaa533239e4b8ee1ca739
Daniel B <danielbakas@gmail.com>
2023-12-05 18:48:49 -0600

    🧹 Chore: Add setup for Jest

---
+++ jest.setup.js
@@ -0,0 +1,16 @@
+/*
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `jest.setup.js`
+ * client | Semantyk
+ *
+ * Created: Dec 05, 2023
+ * Modified: Dec 05, 2023
+ *
+ * Author(s): Semantyk Team
+ * Maintainer(s):
+ *
+ * Copyright © Semantyk 2023. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+import "@testing-library/jest-dom";
