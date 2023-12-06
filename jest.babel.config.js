5d6408c325996579d5863cac18b37b739c6e5db3
Daniel B <danielbakas@gmail.com>
2023-12-05 18:47:15 -0600

    🧹 Chore: Add config for Babel-Jest

---
+++ jest.babel.config.js
@@ -0,0 +1,18 @@
+/*
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `jest.babel.config.js`
+ * client | Semantyk
+ *
+ * Created: Dec 05, 2023
+ * Modified: Dec 05, 2023
+ *
+ * Author(s): Semantyk Team
+ * Maintainer(s):
+ *
+ * Copyright © Semantyk 2023. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+module.exports = {
+    presets: ["next/babel"],
+};
