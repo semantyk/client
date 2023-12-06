ebb6ee0e5e42df600246cb5b39351f84e271f83a
Daniel B <danielbakas@gmail.com>
2023-12-05 18:48:14 -0600

    🧹 Chore: Add config for Jest

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/*
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `jest.config.js`
+ * client | Semantyk
+ *
+ * Created: Dec 05, 2023
+ * Modified: Dec 05, 2023
+ *
+ * Author(s): Semantyk Team
+ * Maintainer(s):
+ *
+ * Copyright © Semantyk 2023. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+module.exports = {
+    testEnvironment: "jest-environment-jsdom",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { configFile: "./jest.babel.config.js" }],
+    },
+    moduleNameMapper: {
+        "\\.(scss|css|sass)$": "identity-obj-proxy",
+        "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    }
+};
