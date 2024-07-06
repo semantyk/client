8e75d67bf3be31c691c60674aabc0343147c1ecd
Daniel B <danielbakas@gmail.com>
2024-07-06 14:56:40 -0600

    🐛 Fix: Remove favicon, SVGs and opengraph images from manifest  - "Add to Home Screen" was loading the favicon instead of the correct icon

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -32,66 +32,13 @@ export default async function manifest() {
         background_color: "#fff",
         theme_color: "#fff",
         icons: [
-            // Favicon
-            {
-                src: "/favicon.svg",
-                sizes: "any",
-                type: "image/svg+xml",
-            },
-            {
-                src: "/favicon.ico",
-                sizes: "any",
-                type: "image/x-icon",
-            },
-            {
-                src: "/favicon-dark.svg",
-                sizes: "any",
-                type: "image/svg+xml",
-            },
-            {
-                src: "/favicon-dark.ico",
-                sizes: "any",
-                type: "image/x-icon",
-            },
-            // Icon
-            {
-                src: "/icon.svg",
-                sizes: "any",
-                type: "image/svg+xml",
-            },
             {
                 src: "/icon.png",
                 sizes: "any",
                 type: "image/png",
             },
             {
-                src: "/icon-dark.svg",
-                sizes: "any",
-                type: "image/svg+xml",
-            },
-            {
                 src: "/icon-dark.png",
-                sizes: "any",
-                type: "image/png",
-            },
-            // SEO
-            {
-                src: "/seo.svg",
-                sizes: "any",
-                type: "image/svg+xml",
-            },
-            {
-                src: "/seo.png",
-                sizes: "any",
-                type: "image/png",
-            },
-            {
-                src: "/seo-dark.svg",
-                sizes: "any",
-                type: "image/svg+xml",
-            },
-            {
-                src: "/seo-dark.png",
                 sizes: "any",
                 type: "image/png",
             }
